bd3ca98feead3f76b6d1c4877c2988c7f5da0734
s1f102301718 <s1f102301718@iniad.org>
2025-10-15 16:14:11 +0900

    favoritesのフロント作成

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   { path: "signup", file: "routes/signup/index.tsx" },
+  { path: "favorites", file: "routes/favorites/index.tsx" },
 ] satisfies RouteConfig;
