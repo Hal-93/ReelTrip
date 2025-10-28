cb36d83edea1fa6a52c5305a0a4a935235bdb9c1
s1f102301718 <s1f102301718@iniad.org>
2025-10-29 01:19:02 +0900

    mappin修正

--- app/components/map/mappin.tsx
+++ app/components/map/mappin.tsx
@@ -1,9 +1,5 @@
 // import { Button } from "~/components/ui/button"
-import {
-  Card,
-  CardAction,
-  CardContent,
-} from "~/components/ui/card"
+import { Card, CardAction, CardContent } from "~/components/ui/card";
 
 import travelIllustration from "public/image.png";
 
@@ -11,24 +7,23 @@ export function MapPin() {
   return (
     <Card className="w-40 h-40 flex flex-col items-center">
       <CardAction>
-    
-      {/* <div className="pt-1 pb-1"> */}
+        {/* <div className="pt-1 pb-1"> */}
         <div className="flex items-center justify-between w-full font-bold space-x-2">
           <span className="pl-3">店舗名</span>
           <span className="text-neutral-500 text-sm pr-3">¥1000~</span>
         </div>
-      {/* </div> */}
+        {/* </div> */}
 
         <CardContent>
-            <div className="relative">
+          <div className="relative">
             <img
-            src={travelIllustration}
-            alt="仮画像"
-            className="w-full h-full object-contain rounded-xl"
+              src={travelIllustration}
+              alt="仮画像"
+              className="w-full h-full object-contain rounded-xl"
             />
-        </div>
+          </div>
         </CardContent>
       </CardAction>
     </Card>
-  )
+  );
 }
